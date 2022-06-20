fec7e6277ecc0d94142ba1ffeee027a0aa33b6bb
Pranay Kothari <pranay.kothari109@gmail.com>
2022-06-20 17:12:45 +0530

    changed assign-students input type to array in patch request

--- modules/assignStudentsModule.js
+++ modules/assignStudentsModule.js
@@ -2,18 +2,7 @@ const mentorModel = require("../models/mentorSchema");
 
 module.exports.assignStudents = async (req, res) => {
     try {
-        const mentor = await mentorModel.findById(req.params.id);
-        
-        if (mentor.assigned_students) {
-            let  data = mentor.assigned_students;
-            data.push(req.body.assigned_students);
-            mentor['assigned_students'] = data;
-            mentor.save();
-        } else {
-            let assigned_students = [req.body.assigned_students];
-            mentor['assigned_students'] = assigned_students;
-        }
-
+        const mentor = await mentorModel.findByIdAndUpdate(req.params.id, {...req.body});
         res.send(mentor);
     } catch (err) {
         res.status(500).send(err.message);

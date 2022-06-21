bdced891fe600ff3ced95b9f80c5274076a6e237
Pranay Kothari <pranay.kothari109@gmail.com>
2022-06-21 15:06:22 +0530

    changed err message and findById

--- modules/assignMentorModule.js
+++ modules/assignMentorModule.js
@@ -4,23 +4,20 @@ const studentModel = require("../models/studentSchema");
 module.exports.assignMentorModule = async (req, res) => {
   try {
     //Assigning Mentor to student
-    const student = await studentModel.findOneAndUpdate(
-      { _id: req.params.id },
-      { ...req.body }
-    );
+    const student = await studentModel.findById(req.params.id, { ...req.body });
 
-    //Updating student for Mentor 
+    //Updating student for Mentor
     let mentor = await mentorModel.findOne({ mentor_name: req.body.mentor });
     let assignedStudents = [];
     if (mentor.assigned_students) {
       assignedStudents = mentor.assigned_students;
     }
     assignedStudents.push(student.student_name);
-    mentor['assigned_students'] = assignedStudents;
+    mentor["assigned_students"] = assignedStudents;
     mentor.save();
     res.send(student);
   } catch (err) {
     console.log(err.message);
-    res.status(500).send(err.message);
+    res.status(500).send("Please enter valid mentor");
   }
 };

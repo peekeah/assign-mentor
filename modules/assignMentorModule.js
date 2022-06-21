e933ceb8b86450de654f28d00be7340cef74e7d3
Pranay Kothari <pranay.kothari109@gmail.com>
2022-06-21 16:01:27 +0530

    changed err message and mongoose method

--- modules/assignMentorModule.js
+++ modules/assignMentorModule.js
@@ -4,8 +4,10 @@ const studentModel = require("../models/studentSchema");
 module.exports.assignMentorModule = async (req, res) => {
   try {
     //Assigning Mentor to student
-    const student = await studentModel.findById(req.params.id, { ...req.body });
-
+    let reqData = {...req.body}
+    let student = await studentModel.findByIdAndUpdate(req.params.id, reqData);
+    // await student.save();
+    console.log(student);
     //Updating student for Mentor
     let mentor = await mentorModel.findOne({ mentor_name: req.body.mentor });
     let assignedStudents = [];
@@ -15,7 +17,7 @@ module.exports.assignMentorModule = async (req, res) => {
     assignedStudents.push(student.student_name);
     mentor["assigned_students"] = assignedStudents;
     mentor.save();
-    res.send(student);
+    await res.send(student);
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Please enter valid mentor");
